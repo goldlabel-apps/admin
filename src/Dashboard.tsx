e806ed04a39b3e3f4ba2649cc5f1b218a04890b6
milky <listingslab@gmail.com>
2022-04-06 21:04:01 +1000

    Renderer

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -2,13 +2,27 @@ import * as React from "react";
 // @ts-ignore
 import * as shared from "@listingslab/shared";
 // @ts-ignore
-import { Dialog } from "@mui/material";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 
 export default function Dashboard() {
-  const { useAppSelector, selectAdmin, useAppDispatch, toggleAdmin } = shared;
+  const {
+    useAppSelector,
+    selectAdmin,
+    useAppDispatch,
+    toggleAdmin,
+    InputEmail,
+    InputPassword,
+    InputSubmit,
+    Icon,
+  } = shared;
   const dispatch = useAppDispatch();
   const admin = useAppSelector(selectAdmin);
-  const { open } = admin;
+  const { open, authed } = admin;
 
   const handleClose = () => {
     dispatch(toggleAdmin(false));
@@ -16,12 +30,28 @@ export default function Dashboard() {
 
   return (
     <Dialog
-      fullScreen
+      fullScreen={authed}
+      fullWidth
+      maxWidth="xs"
       open={open}
       onClose={handleClose}
       sx={{ zIndex: 123456 }}
     >
-      <pre>{JSON.stringify(admin, null, 2)}</pre>
+      <DialogTitle>
+        <Icon icon="admin" />
+      </DialogTitle>
+
+      <DialogContent>
+        <InputEmail />
+        <InputPassword />
+      </DialogContent>
+      <DialogActions>
+        <InputSubmit label="Sign in" />
+      </DialogActions>
     </Dialog>
   );
 }
+
+/*
+<pre>{JSON.stringify(admin, null, 2)}</pre>
+*/
