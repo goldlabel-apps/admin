5826f384371c61567f79e330a4271b0a003e11ff
milky <listingslab@gmail.com>
2022-04-06 20:10:36 +1000

    Hooks up to redux

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -5,11 +5,22 @@ import * as shared from "@listingslab/shared";
 import { Dialog } from "@mui/material";
 
 export default function Dashboard() {
-  const { useAppSelector, selectAdmin } = shared;
+  const { useAppSelector, selectAdmin, useAppDispatch, toggleAdmin } = shared;
+  const dispatch = useAppDispatch();
   const admin = useAppSelector(selectAdmin);
+  const { open } = admin;
+
+  const handleClose = () => {
+    dispatch(toggleAdmin(false));
+  };
 
   return (
-    <Dialog fullScreen open>
+    <Dialog
+      fullScreen
+      open={open}
+      onClose={handleClose}
+      sx={{ zIndex: 123456 }}
+    >
       <pre>{JSON.stringify(admin, null, 2)}</pre>
     </Dialog>
   );
